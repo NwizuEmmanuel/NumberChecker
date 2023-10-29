998ac26ba526a80a467a10cfd0c37bf14b475d36
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2023-10-29 22:04:11 +0800

    Add project files.

---
+++ NumberChecker/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+foreach (var number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine("Even");
+    }else
+    {
+        Console.WriteLine("Odd");
+    }
+}
